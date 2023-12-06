b15f7c798c43a60f2d0dcf3b8242f21f6e5c8b02
kriskata <kriskata5000@gmail.com>
2023-12-06 15:55:07 +0200

    Added working AES encryption and decryption. Added JWT support.

---
+++ lib/security/cryptionHandler.dart
@@ -0,0 +1,48 @@
+import 'package:encrypt/encrypt.dart';
+import 'package:jchatapp/main.dart';
+
+class Cryption {
+  static late Key _globalEncryptionKey;
+  late Encrypter algorithm;
+
+  Cryption() {
+    // Key length 128/192/256 can only be bits.
+    _globalEncryptionKey = Key.fromBase64(ConfigStuff.globalEncryptionKey);
+    algorithm = Encrypter(AES(_globalEncryptionKey, mode: AESMode.ecb));
+    //print("Global Encryption Key: ${base64Encode(_globalEncryptionKey)}");
+  }
+
+  String? globalEncrypt(String? input) {
+    if (input == null || input.isEmpty) {
+      return null;
+    }
+
+    return algorithm.encrypt(input).base64;
+  }
+
+  String? userEncrypt(String? input, String? key) {
+    if (input == null || key == null ||
+        input.isEmpty || key.isEmpty) {
+      return null;
+    }
+
+    return Encrypter(AES(Key.fromBase64(key), mode: AESMode.ecb)).encrypt(input).base64;
+  }
+
+  String? globalDecrypt(String? cipherText) {
+    if (cipherText == null) {
+      return null;
+    }
+
+    return algorithm.decrypt64(cipherText);
+  }
+
+  String? userDecrypt(String? cipherText, String? key) {
+    if (cipherText == null || key == null ||
+        cipherText.isEmpty || key.isEmpty) {
+      return null;
+    }
+
+    return Encrypter(AES(Key.fromBase64(key), mode: AESMode.ecb)).decrypt64(cipherText);
+  }
+}
---
+++ lib/security/jwtHandler.dart
@@ -0,0 +1,81 @@
+import 'dart:collection';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:jchatapp/main.dart';
+import 'package:jchatapp/security/cryptionHandler.dart';
+
+class JwtHandle {
+  late Cryption _cryptionHandle;
+  late SecretKey _globalSecretKey;
+
+  JwtHandle() {
+    _globalSecretKey = SecretKey(ConfigStuff.globalSignKey);
+    _cryptionHandle = new Cryption();
+
+    //print("Global Sign Key: " + ConfigStuff.globalSignKey);
+  }
+
+  Map<dynamic, dynamic>? getData(String jwt, String? encryptionKey, String? signKey) {
+    return encryptionKey == null && signKey == null
+        ? getGlobalClaims(_cryptionHandle.globalDecrypt(jwt))
+        : getUserClaims(
+        _cryptionHandle.userDecrypt(jwt, encryptionKey!), signKey);
+  }
+
+  Map<String, Object>? getDataNoEncryption(String? jwt) {
+    final all = getGlobalClaims(jwt);
+    return all != null ? HashMap.from(all) : null;
+  }
+
+  Map<dynamic, dynamic>? getGlobalClaims(String? jwtToken) {  // HS256
+    if (jwtToken == null) {
+      return null;
+    }
+
+
+    Map<dynamic, dynamic>? claims;
+    try {
+      claims = JWT.verify(jwtToken, _globalSecretKey).payload;
+
+    } catch (e) {
+      claims = null;
+    }
+    return claims;
+  }
+
+  Map<dynamic, dynamic>? getUserClaims(String? jwt, String? signKey) {
+    if (jwt == null || signKey == null) {
+      return null;
+    }
+
+    Map<dynamic, dynamic>? claims;
+    try {
+      claims = JWT.verify(jwt, SecretKey(signKey)).payload;
+
+    } catch (e) {
+      claims = null;
+    }
+    return claims;
+  }
+
+  String? generateGlobalJwt(Map<dynamic, dynamic> claims, bool encrypted) {
+
+     return encrypted ?
+     _cryptionHandle.globalEncrypt(JWT(claims).trySign(_globalSecretKey, noIssueAt: true)) :
+     JWT(claims).trySign(_globalSecretKey, noIssueAt: true);
+
+  }
+
+  String? generateUserJwt(Map<dynamic, dynamic> claims, String? signatureKey,
+      String? encryptionKey) {
+    if (signatureKey == null) {
+      return null;
+    }
+
+    if (encryptionKey != null) {
+      return _cryptionHandle.userEncrypt(JWT(claims).trySign(SecretKey(signatureKey), noIssueAt: true), encryptionKey);
+    }
+
+    return JWT(claims).trySign(SecretKey(signatureKey), noIssueAt: true);
+
+  }
+}
